da41a8dcd5ff493fe23171796f784499821e46b9
Amos Oluoch <aoluoch271@gmail.com>
2024-08-09 14:57:26 +0300

    k

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import UserCard from "../components/UserCard";
 import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
 
 function Home() {
   const [users, setUsers] = useState([]);
@@ -23,6 +24,7 @@ function Home() {
       </header>
       <main>
         <h1>Home!</h1>
+        <Outlet />
         {userList}
       </main>
     </>
